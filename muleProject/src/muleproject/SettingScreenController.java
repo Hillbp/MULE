7abddb8eceabfa31a31f719be31dcf8fc70928b4
zguthrie3 <zachguthrie705@gmail.com>
2015-09-13 21:16:23 -0400

    Added slider functionality to all 3 sliders, as well as handling exceptions for OK.

--- muleProject/src/muleproject/SettingScreenController.java
+++ muleProject/src/muleproject/SettingScreenController.java
@@ -5,21 +5,18 @@
  */
 package muleproject;
 
-import java.net.URL;
-import java.util.ResourceBundle;
-import javafx.fxml.Initializable;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Slider;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
-;
+
 /**
  * FXML Controller class
  *
  * @author Zach
  */
-public class SettingScreenController implements Initializable {
+public class SettingScreenController {
 
     @FXML
     private Slider difficultySlider;
@@ -35,8 +32,8 @@ public class SettingScreenController implements Initializable {
     /**
      * Initializes the controller class.
      */
-    @Override
-    public void initialize(URL url, ResourceBundle rb) {
+    @FXML
+    public void initialize() {
 
     }
     
@@ -56,5 +53,55 @@ public class SettingScreenController implements Initializable {
     private void handleOk() {
         okClicked = true;
         dialogStage.close();
+    }
+    
+    /**
+     * Called when the user moves the difficulty slider.
+     */
+    @FXML
+    private void handleDiffSlide() {
+        if (difficultySlider.valueChangingProperty().equals(true)) {
+            if (difficultySlider.valueProperty().equals(1)) {
+                Settings.setDifficulty(Settings.Difficulty.EASY);
+            } else if (difficultySlider.valueProperty().equals(2)) {
+                Settings.setDifficulty(Settings.Difficulty.NORMAL);
+            } else {
+                Settings.setDifficulty(Settings.Difficulty.TOURNAMENT);
+            }
+        }
+    }
+    
+    /**
+     *  Called when the user moves the maptype slider.
+     */
+    @FXML
+    private void handleMapSlide() {
+        if (mapSlider.valueChangingProperty().equals(true)) {
+            if (mapSlider.valueProperty().equals(1)) {
+                Settings.setMapType(Settings.Maptype.STANDARD);
+            } else if (mapSlider.valueProperty().equals(2)) {
+                Settings.setMapType(Settings.Maptype.WETLAND);
+            } else {
+                Settings.setMapType(Settings.Maptype.MOUNTAIN);
+            }
+        }
+    }
+    
+    /**
+     * Called when the user moves the player number slider.
+     */
+    @FXML
+    private void handlePlayerSlide() {
+        if (numPlayerSlider.valueChangingProperty().equals(true)) {
+            if (numPlayerSlider.valueProperty().equals(1)) {
+                Settings.setNumPlayers(1);
+            } else if (numPlayerSlider.valueProperty().equals(2)) {
+                Settings.setNumPlayers(2);
+            } else if (numPlayerSlider.valueProperty().equals(3)) {
+                Settings.setNumPlayers(3);
+            } else {
+                Settings.setNumPlayers(4);
+            }
+        }
     }
 }
