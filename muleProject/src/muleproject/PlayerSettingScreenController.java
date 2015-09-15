f37ff631567f81e0581020faadd085dabd389bac
zguthrie3 <zachguthrie705@gmail.com>
2015-09-15 19:26:11 -0400

    Added basic variables, imports, and back button support to controller class.

--- muleProject/src/muleproject/PlayerSettingScreenController.java
+++ muleProject/src/muleproject/PlayerSettingScreenController.java
@@ -6,9 +6,62 @@
 package muleproject;
 
 /**
- *
+ * Controller for the playerSettingScreen.
  * @author Zach
  */
+import java.io.IOException;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.control.ColorPicker;
+import javafx.scene.control.MenuButton;
+import javafx.scene.control.TextField;
+import javafx.scene.control.Label;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 public class PlayerSettingScreenController {
     
+    @FXML
+    private ColorPicker colorPicker;
+    
+    @FXML
+    private MenuButton racePicker;
+    
+    @FXML
+    private TextField personName;
+    
+    @FXML
+    private Label playerNum;
+    
+    private int numPlayers;
+    private Stage dialogStage = (Stage) personName.getScene().getWindow();
+    
+    /**
+     * Initializes the playerSettingScreen.
+     */
+    @FXML
+    public void initialize() {
+        
+    }
+            
+    /**
+     * Sets the stage of this dialog.
+     * 
+     * @param dialogStage
+     */
+    public void setdialogStage(Stage dialogStage) {
+        this.dialogStage = dialogStage;
+    }
+
+    /**
+     * Called when the user clicks back. Brings up game setting screen.
+     */
+    @FXML
+    private void handleBack() throws IOException {
+        Parent root;
+        root = FXMLLoader.load(getClass().getResource("settingScreen.fxml"));
+        Scene scene = new Scene(root);
+        dialogStage.setScene(scene);
+        dialogStage.show();
+    }
 }
