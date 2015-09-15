4346a44d8cd7d6a3aebc3ec3cc9d8a60df011983
zguthrie3 <zachguthrie705@gmail.com>
2015-09-15 19:40:28 -0400

    Wrote handlers for all race options.

--- muleProject/src/muleproject/PlayerSettingScreenController.java
+++ muleProject/src/muleproject/PlayerSettingScreenController.java
@@ -16,6 +16,7 @@ import javafx.scene.control.ColorPicker;
 import javafx.scene.control.MenuButton;
 import javafx.scene.control.TextField;
 import javafx.scene.control.Label;
+import javafx.scene.paint.Color;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -35,13 +36,16 @@ public class PlayerSettingScreenController {
     
     private int numPlayers;
     private Stage dialogStage = (Stage) personName.getScene().getWindow();
+    private String tempRace;
+    private String tempName;
+    private Color tempColor;
     
     /**
      * Initializes the playerSettingScreen.
      */
     @FXML
     public void initialize() {
-        
+
     }
             
     /**
@@ -63,5 +67,69 @@ public class PlayerSettingScreenController {
         Scene scene = new Scene(root);
         dialogStage.setScene(scene);
         dialogStage.show();
+    }
+    
+    /**
+     * Called when the user selects Packer as its race.
+     */
+    @FXML
+    private void handlePacker() {
+        tempRace = "Packer";
+    }
+    
+    /**
+     * Called when the user selects Spheroid as its race.
+     */
+    @FXML
+    private void handleSpheroid() {
+        tempRace = "Spheroid";
+    }
+    
+    /**
+     * Called when the user selects Humanoid as its race.
+     */
+    @FXML
+    private void handleHumanoid() {
+        tempRace = "Humanoid";
+    }
+    
+    /**
+     * Called when the user selects Leggite as its race.
+     */
+    @FXML
+    private void handleLeggite() {
+        tempRace = "Leggite";
+    }
+    
+    /**
+     * Called when the user selects Flapper as its race.
+     */
+    @FXML
+    private void handleFlapper() {
+        tempRace = "Flapper";
+    }
+    
+    /**
+     * Called when the user selects Bonzoid as its race.
+     */
+    @FXML
+    private void handleBonzoid() {
+        tempRace = "Bonzoid";
+    }
+    
+    /**
+     * Called when the user selects Mechtron as its race.
+     */
+    @FXML
+    private void handleMechtron() {
+        tempRace = "Mechtron";
+    }
+    
+    /**
+     * Called when the user selects Gollumer as its race.
+     */
+    @FXML
+    private void handleGollumer() {
+        tempRace = "Gollumer";
     }
 }
