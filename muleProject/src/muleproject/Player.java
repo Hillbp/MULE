2a79519b8600139566410acedb73b651b2f83ce6
Christa Peet <silverdog7@gmail.com>
2015-09-16 12:27:31 -0400

    Added getters and setters to Player.java

--- muleProject/src/muleproject/Player.java
+++ muleProject/src/muleproject/Player.java
@@ -2,6 +2,7 @@ import javafx.beans.property.StringProperty;
 import javafx.beans.property.SimpleStringProperty;
 
 public abstract class Player {
+
     protected StringProperty type;
     protected StringProperty name;
     protected StringProperty race;
@@ -16,4 +17,51 @@ public abstract class Player {
 
 	/*Here there will be other methods that apply to both Player types,
 		such as health, money, etc.*/
+    public String getType() {
+        return type.get();
+    }
+
+    public StringProperty typeProperty() {
+        return type;
+    }
+
+    public String getName() {
+        return name.get();
+    }
+
+    public StringProperty nameProperty() {
+        return name;
+    }
+
+    public String getRace() {
+        return race.get();
+    }
+
+    public StringProperty raceProperty() {
+        return race;
+    }
+
+    public String getColor() {
+        return color.get();
+    }
+
+    public StringProperty colorProperty() {
+        return color;
+    }
+
+    public void setType(String type) {
+        this.type.set(type);
+    }
+
+    public void setName(String name) {
+        this.name.set(name);
+    }
+
+    public void setRace(String race) {
+        this.race.set(race);
+    }
+
+    public void setColor(String color) {
+        this.color.set(color);
+    }
 }
