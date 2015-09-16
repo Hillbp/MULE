5e496f8b6b2e8176380c0042434ffdb9e888fb52
Zach <zachguthrie705@gmail.com>
2015-09-15 21:34:09 -0400

    Added a Button to the Title Screen
    Commit changes son

---
+++ muleProject/src/muleproject/TitleScreenController.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package muleproject;
+
+import java.io.IOException;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
+/**
+ * Controller for TitleScreen.
+ * @author Zach
+ */
+public class TitleScreenController {
+    
+    private Label title;
+    private Stage dialogStage = (Stage) title.getScene().getWindow();
+            
+    @FXML
+    private void initialize() {
+        
+    }
+    
+    @FXML
+    private void handleBack() throws IOException {
+        Parent root;
+        root = FXMLLoader.load(getClass().getResource("settingScreen.fxml"));
+        Scene scene = new Scene(root);
+        dialogStage.setScene(scene);
+        dialogStage.show();
+    }
+}
