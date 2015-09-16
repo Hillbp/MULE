acedf6b309c1a89a02c7d27153baf267b5363442
Christa Peet <silverdog7@gmail.com>
2015-09-16 12:48:52 -0400

    Edits to allplayers.java

--- muleProject/src/muleproject/AllPlayers.java
+++ muleProject/src/muleproject/AllPlayers.java
@@ -1,8 +1,8 @@
-import java.util.ArrayList;
+import javafx.beans.property.StringProperty;
 
 public class AllPlayers{
 
-	private ArrayList playerList = new ArrayList<Player>(4);
+	private Player[] playerList = new Player[4];
 	private int numPlayers = 0;
 
 	public AllPlayers(int numHumans) {
@@ -10,68 +10,71 @@ public class AllPlayers{
 		//Do we actually need a constructor here?
 	}
 
-	/**
-	 * this method will tell which player is currently
-	 * selecting his options; do this by counting the
-	 * first blank space in the array and adding 1 to
-	 * the place number
-	 * @return int the player number who is choosing his settings
-	 */
-	public int currentPlayerSettingsNum() {
-		//TODO
-        return 0;
-	}
+    /**
+     * this method will tell which player is currently
+     * selecting his options; do this by counting the
+     * first blank space in the array and adding 1 to
+     * the place number
+     * @return int the player number who is choosing his settings
+     */
+    public int currentPlayerSettingsNum() {
+        for (int i = 0; i < 4; i++) {
+            if (playerList[i] == null) {
+                return i + 1;
+            }
+        }
+        return -1;
+    }
 
-	/**
-	 * this method adds a new player to the array
-	 * @param player player to add
-	 */
-	public void addPlayer(HumanPlayer player) {
-		playerList.append(player);
-		numPlayers++;
-	}
+    /**
+     * this method adds a new player to the array
+     * @param player player to add
+     */
+    public void addPlayer(HumanPlayer player) {
+        playerList[currentPlayerSettingsNum()] = player;
+        numPlayers++;
+    }
 
-	/**
-	 * tells how many human players are already in the array
-	 * @return int number of human players already in the array
-	 */
-	public int playerCount() {
-		return numPlayers;
-	}
+    /**
+     * tells how many human players are already in the array
+     * @return int number of human players already in the array
+     */
+    public int playerCount() {
+        return numPlayers;
+    }
 
-	/**
-	 * this method fills the rest of the array
-	 * with CPUs and only executes after all the players
-	 * have been added
-	 *
-	 */
-	public void addCPUs() {
-		int playernum = playerList.playerCount();
-		for (int i = playernum; i < 4; i++) {
-			playerList.append(new AIPlayer());
-		}
-	}
+    /**
+     * this method fills the rest of the array
+     * with CPUs and only executes after all the players
+     * have been added
+     *
+     */
+    public void addCPUs() {
+        int playernum = playerCount();
+        for (int i = playernum - 1; i < 4; i++) {
+            playerList[i] = new AIPlayer();
+        }
+    }
 
-	/**
-	 * this method checks to see if the array already contains
-	 * a player with this name
-	 *
-	 * need to make a getName() method, but where?
-	 *
-	 * @param playerName
-	 * @return boolean whether contains name or not
-	 */
-	public boolean containsName(String playerName) {
-		for (int i = 0; i < numPlayers; i++) {
-			if (playerList[i].getName().equals(playerName)) {
-				return true;
-			} else {
-				return false;
-			}
-		}
-	}
+    /**
+     * this method checks to see if the array already contains
+     * a player with this name
+     *
+     * need to make a getName() method, but where?
+     *
+     * @param playerName name of player
+     * @return boolean whether contains name or not
+     */
+    public boolean containsName(StringProperty playerName) {
+        for (int i = 0; i < numPlayers; i++) {
+            if (playerList[i].getName().equals(playerName)) {
+                return true;
+            }
+        }
+        return false;
+    }
 
-	/*
+    /*
      *this method checks to see if the array already contains
      * a player with this color
      *
@@ -79,15 +82,13 @@ public class AllPlayers{
      *
      * @return boolean whether contains color or not
      */
-	public boolean containsColor(Color playerColor) {
-		for (int i = 0; i < numPlayers; i++) {
-			if (playerList[i].getColor().equals(playerColor)) {
-				return true;
-			} else {
-				return false;
-			}
-		}
-	}
-
+    public boolean containsColor(StringProperty playerColor) {
+        for (int i = 0; i < numPlayers; i++) {
+            if (playerList[i].getColor().equals(playerColor)) {
+                return true;
+            }
+        }
+        return false;
+    }
 
 }
