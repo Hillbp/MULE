f13eb877c3872569825cf31204a0f4b1330820b9
jvinson7 <saxjt@bellsouth.net>
2015-09-17 12:36:46 -0400

    Changed strings to StringProperty.

--- muleProject/src/muleproject/AIPlayer.java
+++ muleProject/src/muleproject/AIPlayer.java
@@ -1,14 +1,16 @@
 package muleproject;
-
+import javafx.beans.property.StringProperty;
+import javafx.beans.property.SimpleStringProperty;
 public class AIPlayer extends Player {
 	
-	public AIPlayer(String name, String race, String color) {
+	public AIPlayer(StringProperty name, StringProperty race, StringProperty color) {
 		super(name, race, color, "AI");
 	}
 
 	public AIPlayer() {
 		//TODO change race to actual race
-		this("comp", "race", "blue");
+		this(new SimpleStringProperty("comp"), new SimpleStringProperty(
+                        "race"), new SimpleStringProperty("blue"));
 	}
 
 	/*Methods that only relate to ai players go here*/
