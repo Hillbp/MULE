c2c74841f006ab0ea618de8723b7b4de2feb6a3d
Zach Guthrie <zachguthrie705@gmail.com>
2015-09-24 12:45:08 -0400

    Creation of Property file
    Created Property class with elements type, tile number, value, and
    bought status.

---
+++ src/main/java/muleproject/Property.java
@@ -0,0 +1,48 @@
+public class Property {
+    private int propNum;
+    private int propValue;
+    private String propType;
+    private boolean isBought = false;
+
+    public Property(int num, int value, String type) {
+        propNum = num;
+        propValue = value;
+        propType = type;
+    }
+
+    public int getNum() {
+        return propNum;
+    }
+
+    public int getValue() {
+        return propValue;
+    }
+
+    public String getType() {
+        return propType;
+    }
+
+    public void setNum(int newNum) {
+        propNum = newNum;
+    }
+
+    public void setValue(int value) {
+        propValue = value;
+    }
+
+    public void setType(String type) {
+        propType = type;
+    }
+
+    public boolean isBought() {
+        return isBought;
+    }
+
+    public void toggleBought() {
+        if (!isBought) {
+            isBought = true;
+        } else {
+            isBought = false; 
+        }
+    }
+}
