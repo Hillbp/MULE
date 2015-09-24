f07808754976019711050aafba302832990a5211
Christa Peet <silverdog7@gmail.com>
2015-09-24 17:45:31 -0400

    added gridScreenController.java and TownScreenController.java

---
+++ src/main/java/muleproject/TownScreenController.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package muleproject;
+
+import java.io.IOException;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
+/**
+ * Controller for TownScreen.
+ * @author Christa
+ */
+public class TownScreenController {
+
+    private Label title;
+    private Stage dialogStage = (Stage) title.getScene().getWindow();
+
+    @FXML
+    private void initialize() {
+
+    }
+
+    @FXML
+    private void handleBack() throws IOException {
+        Parent root;
+        root = FXMLLoader.load(getClass().getResource("workInProgressScreen.fxml"));
+        Scene scene = new Scene(root);
+        dialogStage.setScene(scene);
+        dialogStage.show();
+    }
+}
