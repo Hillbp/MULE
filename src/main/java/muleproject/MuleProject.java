b707b539c6f29731519232fcf50ff5b30944b06b
Christa Peet <silverdog7@gmail.com>
2015-09-29 00:40:59 -0400

    updated gitignore and added TODO

--- src/main/java/muleproject/MuleProject.java
+++ src/main/java/muleproject/MuleProject.java
@@ -70,14 +70,14 @@ public class MuleProject extends Application {
         boolean landCheck;
         //Goes through every player's turn
         for (int i = 0; i < 4; i++) {
-            //This method gets the player at an index in the array. 
-            currentPlayer = players.getPlayer(i);
+            //This method gets the player at an index in the array.
+            currentPlayer = players.getPlayer(i); //TODO does not exist
             if (currentPlayer.getType().equals("Human")) {
                 //TODO: Listen for button press
             } else {
                 int landNumValue = (int) ((Math.random() * 45) + 1);
                 // Need static method that allows a property to be selected from the board.
-                Property landChoice = MapScreenController.getTile(landNumValue); 
+                Property landChoice = MapScreenController.getTile(landNumValue);
                 if (!landChoice.isBought()) {
                     landChoice.setOwner(currentPlayer);
                     landChoice.toggleBought();
@@ -93,7 +93,7 @@ public class MuleProject extends Application {
     public void initLandPurchase() {
         boolean landCheck;
         for (int i = 0; i < 4; i++) {
-            //This method gets the player at an index in the array. 
+            //This method gets the player at an index in the array.
             currentPlayer = players.getPlayer(i);
             if (currentPlayer.getType().equals("Human")) {
                 //TODO: Listen for button press
