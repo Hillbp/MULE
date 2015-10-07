c7a1a53449255112ec34d558c966cb6f048324ce
Hillbp <brandonphill1@gmail.com>
2015-10-06 22:29:56 -0400

    Created Turn.nextTurn() to increment turn number

--- src/main/java/muleproject/Turn.java
+++ src/main/java/muleproject/Turn.java
@@ -1,10 +1,13 @@
 import java.util.PriorityQueue;
 
 public class Turn {
-    private Player current;
     private int turnNumber;
 
     public int getTurnNumber () {
         return turnNumber;
+    }
+
+    public void nextTurn() {
+        turnNumber++;
     }
 }
