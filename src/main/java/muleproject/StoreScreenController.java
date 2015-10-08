abf027be6774d0053153d8df30afa5dc4cf1b0b8
Zach <zachguthrie705@gmail.com>
2015-10-08 16:14:46 -0400

    Gave code and fxml identifies to all objects on store screen
    Read title

--- src/main/java/muleproject/StoreScreenController.java
+++ src/main/java/muleproject/StoreScreenController.java
@@ -7,6 +7,8 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.stage.Stage;
 import javafx.scene.control.Label;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 
 import java.io.IOException;
 
@@ -38,6 +40,10 @@ public class StoreScreenController {
     private void initialize() {
         currentTurn = GridScreenController.turn;
         currentPlayer = MuleProject.players.getPlayer(currentTurn.getTurnNumber() % 4);
+        energy.setText("Current energy: " + currentPlayer.getEnergy());
+        food.setText("Current food: " + currentPlayer.getFood());
+        smithore.setText("Current smithore: " + currentPlayer.getSmithore());
+        cash.setText("Current cash: " + currentPlayer.getMoney());
     }
 
     @FXML
@@ -49,5 +55,91 @@ public class StoreScreenController {
         Scene scene = new Scene(root);
         dialogStage.setScene(scene);
         dialogStage.show();
+    }
+
+    @FXML
+    private void handleBuyFood() {
+        Stage dialogStage;
+        dialogStage = (Stage) playerNum.getScene().getWindow();
+        if (currentPlayer.getMoney() - 5 > 0) {
+            currentPlayer.setMoney(currentPlayer.getMoney() - 5);
+            currentPlayer.setFood(currentPlayer.getFood() + 1);
+            food.setText("Current food: " + currentPlayer.getFood());
+            cash.setText("Current cash: " + currentPlayer.getMoney());
+        } else {
+            Alert alert = new Alert(AlertType.ERROR);
+            alert.initOwner(dialogStage);
+            alert.setTitle("Not Enough Money");
+            alert.setHeaderText("Not Enough Money for Transcation");
+            
+            alert.showAndWait();
+        }
+    }
+
+    @FXML
+    private void handleSellFood() {
+        Stage dialogStage;
+        dialogStage = (Stage) playerNum.getScene().getWindow();
+        if (currentPlayer.getFood() > 0) {
+            currentPlayer.setFood(currentPlayer.getFood() - 1);
+            currentPlayer.setMoney(currentPlayer.getMoney() + 5);
+            food.setText("Current food: " + currentPlayer.getFood());
+            cash.setText("Current cash: " + currentPlayer.getMoney());
+        } else {
+            Alert alert = new Alert(AlertType.ERROR);
+            alert.initOwner(dialogStage);
+            alert.setTitle("Not Enough Food");
+            alert.setHeaderText("Not Enough Food for Transcation");
+            
+            alert.showAndWait();
+        }
+    }
+
+    @FXML
+    private void handleBuyEnergy() {
+        Stage dialogStage;
+        dialogStage = (Stage) playerNum.getScene().getWindow();
+        if (currentPlayer.getMoney() - 10 > 0) {
+            currentPlayer.setMoney(currentPlayer.getMoney() - 10);
+            currentPlayer.setEnergy(currentPlayer.getEnergy() + 1);
+            energy.setText("Current energy: " + currentPlayer.getEnergy());
+            cash.setText("Current cash: " + currentPlayer.getMoney());            
+        } else {
+            Alert alert = new Alert(AlertType.ERROR);
+            alert.initOwner(dialogStage);
+            alert.setTitle("Not Enough Money");
+            alert.setHeaderText("Not Enough Money for Transcation");
+            
+            alert.showAndWait();
+        }
+    }
+
+    @FXML
+    private void handleSellEnergy() {
+        Stage dialogStage;
+        dialogStage = (Stage) playerNum.getScene().getWindow();
+        if (currentPlayer.getEnergy() > 0) {
+            currentPlayer.setEnergy(currentPlayer.getEnergy() - 1);
+            currentPlayer.setMoney(currentPlayer.getMoney() + 10);
+            food.setText("Current food: " + currentPlayer.getFood());
+            cash.setText("Current cash: " + currentPlayer.getMoney());
+        } else {
+            Alert alert = new Alert(AlertType.ERROR);
+            alert.initOwner(dialogStage);
+            alert.setTitle("Not Enough Energy");
+            alert.setHeaderText("Not Enough Energy for Transcation");
+            
+            alert.showAndWait();
+        }
+    }
+
+    @FXML
+    private void handleBuyMule() {
+
+    }
+
+    @FXML
+    private void handleSellMule() {
+
     }
 }
